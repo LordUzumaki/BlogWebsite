51773ef2c2c1885cd033e5c023cc099e0fcf466f
lorduzumaki <alemuteddy@gmail.com>
2024-08-02 21:13:29 -0600

    worked

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_hard_to_guess_string'
-    SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL') or 'sqlite://site.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
-    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- model.py
+++ model.py
@@ -1,15 +1,9 @@
-from datetime import datetime, timezone
+from datetime import datetime
+from extensions import db
 from flask_login import UserMixin
-from model import db, login_manager
-
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, Primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
@@ -17,15 +11,14 @@ class User(db.Model, UserMixin):
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return f"User ('{self.username}', '{self.email}', '{self.image_file}'"
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 class Post(db.Model):
-    id = db.Column(db.Integer, Primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.Datetime, nullable=False, default = datetime.now(timezone.utcnow))
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user_id'), nullable=False)
-    
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted})"
-    
+        return f"Post('{self.title}', '{self.date_posted}')"
