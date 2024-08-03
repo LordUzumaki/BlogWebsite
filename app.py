f6570266455903f28349db4bc43f0078a4c17f9f
lorduzumaki <alemuteddy@gmail.com>
2024-08-02 21:11:37 -0600

    finally fixed somestuff

--- app.py
+++ app.py
@@ -1,2 +1,3 @@
-import flask 
-from flask import Flask_SQLAlchemy
+from flask import Flask, render_template,  url_for, flash, redirect, request
+from flask_sqlalchemy  import SQLAlchemy
+from flask_bcrypt import Bcrypt
